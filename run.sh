67b0cef22a900d6a73e5f9da2e5a1aa58edae70f
Christian Carlé <christian.carle@volvocars.com>
2019-07-25 11:49:37 +0200

    removed copy

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ DB_SIZE=`du -m /mnt/db | cut -f 1`
 #if [ "$ACTIVESTATE" = "ActiveState=active" ] && [ "$SUBSTATE" = "SubState=running" ] && [ $MINSIZE -le $DB_SIZE ] && grep -qs '/nas_etcd' /proc/mounts
 #then
 mkdir -p /nas_etcd/etcd_backup/`hostname -s`/etcd-config-$(date +%Y%m%d)/
-cp -R /etc/etcd/ /nas_etcd/etcd_backup/`hostname -s`/etcd-config-$(date +%Y%m%d)/
+#cp -R /etc/etcd/ /nas_etcd/etcd_backup/`hostname -s`/etcd-config-$(date +%Y%m%d)/
 mkdir -p /nas_etcd/etcd_backup/`hostname -s`/etcd-data-$(date +%Y%m%d)
 #ETCDCTL_API=3 etcdctl --cert="/etc/etcd/peer.crt" --key="/etc/etcd/peer.key" --cacert="/etc/etcd/ca.crt"  --endpoints https://`hostname`:2379 snapshot save /nas_etcd/etcd_backup/`hostname -s`/etcd-data-$(date +%Y%m%d)/db
 #else
